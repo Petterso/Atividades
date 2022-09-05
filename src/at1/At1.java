99d08c37e33880c3ae7b763785c5795ac4a15204
Petterson <petterson.1998@hotmail.com>
2022-09-05 13:03:09 -0300

    Primeira parte da atividade de POO

---
+++ src/at1/At1.java
@@ -0,0 +1,30 @@
+package at1;
+
+import java.util.Scanner;
+
+public class At1 {
+
+	public static boolean valida(Double a, Double b) {
+		if (a == 0 || b == 0) {
+			return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		double precoLitroPrimitivo = 0, quantidadeLitroPrimitivo = 0;
+		Scanner leitura = new Scanner(System.in);
+		Double precoLitro = Double.valueOf(precoLitroPrimitivo);
+		Double quantidadeLitro = Double.valueOf(quantidadeLitroPrimitivo);
+		System.out.println("Digite o valor de litro e depois a quantidade de litros de combustivel");
+		precoLitro = leitura.nextDouble();
+		quantidadeLitro = leitura.nextDouble();
+		
+		if (valida(precoLitro, quantidadeLitro)) {
+			System.out.println(precoLitro * quantidadeLitro);
+		} else {
+			System.out.println("Pelo menos um dos valores não é válido");
+		}
+		leitura.close();
+	}
+}
